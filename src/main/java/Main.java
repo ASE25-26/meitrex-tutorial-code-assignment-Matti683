5a0b5f73ffa8f9c5e7cf210d975616082325646c
BuildTools <unconfigured@null.spigotmc.org>
2025-10-23 18:48:55 +0200

    Hello MEITREX Aufgabe gelöst

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello MEITREX");
 	System.out.println("Hello MEITREX");
         // TODO: Gib "Hello MEITREX" auf der Konsole aus
     }
